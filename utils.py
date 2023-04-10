a42720f68287bba0ddc5256a67dd35108a24e077
Delon Liu Shen <delon@sh02-03n56.int>
2023-04-10 01:31:27 -0700

    started trying to MCMC fit mass function

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 import math
-from scipy.integrate import quad
+from scipy.integrate import quad, fixed_quad
 import matplotlib.pyplot as plt
 import pickle 
 import numpy as np
 
-G = 4.3009e-9 #km^2 Mpc/ (Msolar  s^2)
+G = 4.3009e-9 #km^2 Mpc/ (Msolar  s^2) weird units to make rhom_a good units 
+cosmo_params = pickle.load(open('cosmo_params.pkl', 'rb'))
 
 def M_to_R(M, box, a):
     return (M / (4/3 * math.pi * rhom_a(box, a))) ** (1/3) # h^-1 Mpc  
@@ -24,12 +25,11 @@ def sigma2(pk, R):
         W = (3 / x) * (np.sin(x) / x**2 - np.cos(x) / x)
         dσ2dk = W**2 * pk(k) * k**2 / 2 / np.pi**2
         return dσ2dk
-    res, err = quad(dσ2dk, 0, 20 / R)
+    res, err = fixed_quad(dσ2dk, 0, 20 / R)
     σ2 = res
     return σ2
 
 def rhom_a(box, a):
-    cosmo_params = pickle.load(open('cosmo_params.pkl', 'rb'))
     ombh2 = cosmo_params[box]['ombh2']
     omch2 = cosmo_params[box]['omch2']
     H0 = cosmo_params[box]['H0'] #[km s^-1 Mpc-1]
