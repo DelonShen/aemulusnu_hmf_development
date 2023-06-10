15c20095cc0f4651e80f04bf7b34399c7c7cdfa2
Delon Liu Shen <delonshen@gmail.com>
2023-06-09 20:19:10 -0700

    looking into why box5 fitting weird, think it might be due to weird covariance matrix?

--- fit_individ_iter_handler.py
+++ fit_individ_iter_handler.py
@@ -1,4 +1,5 @@
-box = 'Box_n50_0_1400'
+box_prev = 'Box_n50_0_1400'
+box = 'Box5_1400'
 from utils import *
 from massfunction import *
 
@@ -32,9 +33,34 @@ import subprocess
 from datetime import date
 
 
+
+line = 'python -u fit_individ_iter.py %s %f %s %f'%(box, 1.0, box_prev, 1.0)
+print(line)
+# Define the job parameters
+job_name = 'fit_%s_a%.2f'%(box, 1.0)
+
+# Get the current date
+current_date = date.today().strftime('%Y-%m-%d')
+
+# Construct the output and error log paths
+output_log = f'logs/{current_date}-{job_name}.out'
+error_log = f'logs/{current_date}-{job_name}.err'
+sbatch_command = f'sbatch --job-name={job_name} --output={output_log} --error={error_log} --time=5:00 -p kipac --nodes=1 --mem=32768 --cpus-per-task=1 --wrap="{line}"'
+
+
+subprocess.run(sbatch_command, shell=True)
+
+# Wait for the current job to finish
+while True:
+    squeue_output = subprocess.check_output(['squeue', '-u', 'delon', '-n', job_name, '-h']).decode().strip()
+    if not squeue_output:
+        break
+        
+        
 # Submit each command as a Slurm job with the dynamically generated log paths    
 for i in trange(1, len(a_list)):
     line = 'python -u fit_individ_iter.py %s %f %s %f'%(box, a_list[i], box, a_list[i-1])
+    print(line)
     # Define the job parameters
     job_name = 'fit_%s_a%.2f'%(box, a_list[i])
 
