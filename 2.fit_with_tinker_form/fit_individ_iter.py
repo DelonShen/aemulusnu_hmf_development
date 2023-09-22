refitting with now correct jackknifed covariance

2.fit_with_tinker_form/fit_individ_iter.py
@@ -8,7 +8,7 @@ prev_box = sys.argv[3]
 prev_a = eval(sys.argv[4])
 
 
-KX = np.diag([1e-5, 1e-5, 1e-6, 1e-5])
+KX = np.diag([1e-2, 1e-2, 1e-2, 1e-2])
 # KX = np.diag([1e-5, 1e-5, 1e-6, 1e-3])
 
 # if(a_fit == 1.0):